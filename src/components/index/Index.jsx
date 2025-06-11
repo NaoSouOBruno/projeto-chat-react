da82bad88e9ee1d335788cb5721189c98faa3309
Bruno Pagani Rampinelli <brunorampinelli2006@gmail.com>
2025-06-11 15:04:02 -0300

    Início Index

--- src/components/index/Index.jsx
+++ src/components/index/Index.jsx
@@ -1,10 +1,22 @@
+import { Link } from 'react-router-dom'
+
 import './Index.css'
 
 export function Index() {
 
   return (
     <>
-      <p> INDEX </p>
+      <div className='indexBody'>
+
+        <h1>Bem-vindo à central de atendimento!</h1>
+        <p>Digite seu nome para prosseguir:</p>
+
+        <form>
+          <input type='text' required/>
+          <Link to='chat'><input type='submit' value='Entrar'></input></Link>
+        </form>
+
+      </div>
     </>
   )
 }
